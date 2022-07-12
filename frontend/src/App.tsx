01e09bbe4e30c6cdd1773f526e66f42927cc504e
Deivison06 <santosdeivison764@gmail.com>
2022-07-12 10:27:48 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./Components/notification-buttom"
+
 function App() {
-  return(
-    <h1>Ola mundo</h1>
+  return (
+    <>
+      <h1>Ola mundo</h1>
+      <NotificationButton />
+    </>
+
   )
 }
 
